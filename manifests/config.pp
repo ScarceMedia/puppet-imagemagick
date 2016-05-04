6463523ff4570da6b7ac67c4f951007a12a5a6dd
Simon Rondelez <simonrondelez@me.com>
2016-05-04 11:40:46 +0200

    Fix typo

--- manifests/config.pp
+++ manifests/config.pp
@@ -14,7 +14,7 @@ class imagemagick::config {
     file { '/etc/ImageMagick/policy.xml':
       ensure => file,
       mode   => '0644',
-      source => 'puppet:///modules/imagemagick/etc/ImageMagick/policy.xml'
+      source => 'puppet:///modules/imagemagick/etc/ImageMagick/policy.xml',
       notify => service['apache2'],
     }
   }
