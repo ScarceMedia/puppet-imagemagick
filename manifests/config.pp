7c982517af815390a209c6d18444fb12ad2bd6dc
Simon Rondelez <simonrondelez@me.com>
2016-05-04 10:16:44 +0200

    Also watch for config changes

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,10 +1,19 @@
 # == Class: imagemagick::config
 #
 class imagemagick::config {
-  # resources
-  file { '/etc/ImageMagick/policy.xml':
-    ensure => file,
-    mode   => '0644',
-    source => 'puppet:///modules/imagemagick/etc/ImageMagick/policy.xml'
+  if defined(Class['php::fpm']) {
+    file { '/etc/ImageMagick/policy.xml':
+      ensure => file,
+      mode   => '0644',
+      source => 'puppet:///modules/imagemagick/etc/ImageMagick/policy.xml'
+    }
+  }
+
+  if defined(Class['apache2']) {
+    file { '/etc/ImageMagick/policy.xml':
+      ensure => file,
+      mode   => '0644',
+      source => 'puppet:///modules/imagemagick/etc/ImageMagick/policy.xml'
+    }
   }
 }
