040b0012c68b75b019602a0477b93484ec7046aa
Simon Rondelez <simonrondelez@me.com>
2016-05-04 13:46:05 +0200

    Remove defines

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,21 +1,9 @@
 # == Class: imagemagick::config
 #
 class imagemagick::config {
-  if defined(Service['php5-fpm']) {
-    file { '/etc/ImageMagick/policy.xml':
-      ensure => file,
-      mode   => '0644',
-      source => 'puppet:///modules/imagemagick/etc/ImageMagick/policy.xml',
-      notify => service['php5-fpm'],
-    }
-  }
-
-  if defined(Service['apache2']) {
-    file { '/etc/ImageMagick/policy.xml':
-      ensure => file,
-      mode   => '0644',
-      source => 'puppet:///modules/imagemagick/etc/ImageMagick/policy.xml',
-      notify => service['apache2'],
-    }
+  file { '/etc/ImageMagick/policy.xml':
+    ensure => file,
+    mode   => '0644',
+    source => 'puppet:///modules/imagemagick/etc/ImageMagick/policy.xml',
   }
 }
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,18 +1,7 @@
 # == Class: imagemagick::install
 #
 class imagemagick::install {
-  if defined(Class['php::fpm']) {
-    package {$::imagemagick::params::packages:
-      ensure => latest,
-      notify => service['php5-fpm']
-    }
+  package {$::imagemagick::params::packages:
+    ensure => latest,
   }
-
-  if defined(Class['apache2']) {
-    package {$::imagemagick::params::packages:
-      ensure => latest,
-      notify => service['apache2']
-    }
-  }
-
 }
