b3bc542b35d01aec3a579185f212fcbb9718a038
Simon Rondelez <simonrondelez@me.com>
2016-05-04 11:39:22 +0200

    Fix defined method

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,19 +1,21 @@
 # == Class: imagemagick::config
 #
 class imagemagick::config {
-  if defined(Class['php::fpm']) {
+  if defined(Service['php5-fpm']) {
     file { '/etc/ImageMagick/policy.xml':
       ensure => file,
       mode   => '0644',
-      source => 'puppet:///modules/imagemagick/etc/ImageMagick/policy.xml'
+      source => 'puppet:///modules/imagemagick/etc/ImageMagick/policy.xml',
+      notify => service['php5-fpm'],
     }
   }
 
-  if defined(Class['apache2']) {
+  if defined(Service['apache2']) {
     file { '/etc/ImageMagick/policy.xml':
       ensure => file,
       mode   => '0644',
       source => 'puppet:///modules/imagemagick/etc/ImageMagick/policy.xml'
+      notify => service['apache2'],
     }
   }
 }
